f00b23dcfefa85a84718b9bc221a2eeeea749875
Admin <rujji@Rujjis-MacBook-Air.local>
2021-10-25 15:49:04 +0700

    Initial Commit

--- src/main/java/com/ttb/fleet/notification/common/dto/ResponseOut.java
+++ src/main/java/com/ttb/fleet/notification/common/dto/ResponseOut.java
@@ -1,2 +1,33 @@
-package com.ttb.fleet.notification.common.dto;public class ResponseOut {
+package com.ttb.fleet.notification.common.dto;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+@JsonIgnoreProperties
+public class ResponseOut implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @JsonProperty("api_status")
+    private ApiStatusOut apiStatus = new ApiStatusOut();
+    private transient Map<String, Object> data = new HashMap<>();
+
+    public ApiStatusOut getApiStatus() {
+        return apiStatus;
+    }
+
+    public void setApiStatus(ApiStatusOut apiStatus) {
+        this.apiStatus = apiStatus;
+    }
+
+    public Map<String, Object> getData() {
+        return data;
+    }
+
+    public void setData(Map<String, Object> data) {
+        this.data = data;
+    }
 }

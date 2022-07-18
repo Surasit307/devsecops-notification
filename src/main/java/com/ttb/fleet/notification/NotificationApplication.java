fabe73c9ac08f5b3bb58269b16a9c065220b2721
Thitiwat Wuthimapakorn <thitiwat.wuthimapakorn@apac.nttdata.com>
2022-07-18 11:09:14 +0700

    edit for devsecops

--- src/main/java/com/ttb/fleet/notification/NotificationApplication.java
+++ src/main/java/com/ttb/fleet/notification/NotificationApplication.java
@@ -2,10 +2,9 @@ package com.ttb.fleet.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class NotificationApplication {
 
 	public static void main(String[] args) {
